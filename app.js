b8f11acf4c633e9634d569be07479817e2792076
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-09-29 16:34:31 -0400

    Getting user input

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+const command = process.argv[2];
+
+console.log('Command', command);
+console.log(process.argv);
+
+if (command === 'add') {
+    console.log('Adding new note')
+} else if (command === 'list') {
+    console.log('Listing all notes')
+} else if (command === 'read') {
+    console.log('Fetching note')
+} else if (command === 'remove') {
+    console.log('Remove note')
+} else {
+    console.log('Command not recognised');
+}
