a749110d8d0bc5259d0db7bc9c4c155c7d230820
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-01 09:22:42 -0400

    listing notes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,8 +6,6 @@ const notes = require('./notes');
 
 const argv = yargs.argv;
 const command = process.argv[2];
-console.log('Command', command);
-console.log('Yargs', argv);
 
 if (command === 'add') {
     const note = notes.addNote(argv.title, argv.body);
@@ -20,7 +16,9 @@ if (command === 'add') {
         console.log('Note title already in use');
     }
 } else if (command === 'list') {
-    notes.getAll();
+   const allNotes = notes.getAll();
+   console.log(`Printing ${allNotes.length} notes(s).`);
+   allNotes.forEach(note => notes.logNote(note))
 } else if (command === 'read') {
     const targetNote = notes.getNote(argv.title);
     if (targetNote) {
