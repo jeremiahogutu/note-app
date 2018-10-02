020174970a3a1d9a272f8f72ca258ec203ee2f31
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-10-01 22:45:46 -0400

    refactored to switch instead of if/else

--- app.js
+++ app.js
@@ -30,30 +30,36 @@ const argv = yargs
     .argv;
 const command = process.argv[2];
 
-if (command === 'add') {
-    const note = notes.addNote(argv.title, argv.body);
-    if (note) {
-        console.log('Note created');
-        notes.logNote(note)
-    } else {
-        console.log('Note title already in use');
-    }
-} else if (command === 'list') {
-    const allNotes = notes.getAll();
-    console.log(`Printing ${allNotes.length} notes(s).`);
-    allNotes.forEach(note => notes.logNote(note))
-} else if (command === 'read') {
-    const targetNote = notes.getNote(argv.title);
-    if (targetNote) {
-        console.log('Found Note');
-        notes.logNote(targetNote)
-    } else {
-        console.log('Note was not found');
-    }
-} else if (command === 'remove') {
-    const noteRemoved = notes.removeNote(argv.title);
-    const message = noteRemoved ? 'Note was removed' : 'Note not found';
-    console.log(message);
-} else {
-    console.log('Command not recognised');
+switch (command) {
+    case 'add':
+        const note = notes.addNote(argv.title, argv.body);
+        if (note) {
+            console.log('Note created');
+            notes.logNote(note)
+        } else {
+            console.log('Note title already in use');
+        }
+        break;
+    case 'list':
+        const allNotes = notes.getAll();
+        console.log(`Printing ${allNotes.length} notes(s).`);
+        allNotes.forEach(note => notes.logNote(note));
+        break;
+    case 'read':
+        const targetNote = notes.getNote(argv.title);
+        if (targetNote) {
+            console.log('Found Note');
+            notes.logNote(targetNote)
+        } else {
+            console.log('Note was not found');
+        }
+        break;
+    case 'remove':
+        const noteRemoved = notes.removeNote(argv.title);
+        const message = noteRemoved ? 'Note was removed' : 'Note not found';
+        console.log(message);
+        break;
+    default:
+        console.log('Command not recognised');
+        break;
 }
